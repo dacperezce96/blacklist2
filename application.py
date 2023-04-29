fac20fbd9aad65945cc9fdc5303daab03ac04a17
Camilo <dc.perezc1@uniandes.edu.co>
2023-04-29 12:42:05 -0500

    first commit

---
+++ application.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from modelos.modelos import db
+from vistas.vistas import VistaBlackList, VistaBlackUser
+from flask_restful import Api
+
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///test.db")
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app_context = application.app_context()
+app_context.push()
+
+db.init_app(application)
+db.create_all()
+
+api = Api(application)
+api.add_resource(VistaBlackList, '/blacklists')
+api.add_resource(VistaBlackUser, '/blacklists/<string:email>')
+
+@application.route("/ping")
+def index():
+    return "pong",200
+
+if __name__ == "__main__":
+    application.run(port = 5000, debug = True)
