60695de89174c0593d98c6aff0a91e2ab06d0870
Camilo <dc.perezc1@uniandes.edu.co>
2023-05-14 13:57:03 -0500

    entrega continua fallido

--- tests/test_main.py
+++ tests/test_main.py
@@ -25,36 +25,36 @@ class TestUtilidades(TestCase):
         db.session.delete(self.user)
         db.session.commit()
 
-    # def test_crear_black_user(self):
-    #     nuevo_usuario ={            
-    #         "email": self.data_factory.email(),
-    #         "app_uuid": self.data_factory.uuid4(),
-    #         "blocked_reason": self.data_factory.text()
-    #     }
-    #     solicitud_nuevo_usuario = self.client.post(
-    #         "/blacklists",
-    #         data = json.dumps(nuevo_usuario),
-    #         headers={'Content-Type': 'application/json',
-    #                  'Authorization': 'Bearer token'}
-    #     )
-    #     self.assertEqual(solicitud_nuevo_usuario.status_code, 201)
-    #     id = solicitud_nuevo_usuario.json['entidad']['id']
-    #     usuario = BlackUser.query.get(id)                
-    #     db.session.delete(usuario)
-    #     db.session.commit()
+    def test_crear_black_user(self):
+        nuevo_usuario ={            
+            "email": self.data_factory.email(),
+            "app_uuid": self.data_factory.uuid4(),
+            "blocked_reason": self.data_factory.text()
+        }
+        solicitud_nuevo_usuario = self.client.post(
+            "/blacklists",
+            data = json.dumps(nuevo_usuario),
+            headers={'Content-Type': 'application/json',
+                     'Authorization': 'Bearer token'}
+        )
+        self.assertEqual(solicitud_nuevo_usuario.status_code, 201)
+        id = solicitud_nuevo_usuario.json['entidad']['id']
+        usuario = BlackUser.query.get(id)                
+        db.session.delete(usuario)
+        db.session.commit()
     
 
-    # def test_consultar_black_user(self):
-    #     solicitud_nuevo_usuario = self.client.get(
-    #         f"/blacklists/{self.email}",
-    #         headers={'Authorization': 'Bearer token'}
-    #     )
-    #     self.assertEqual(solicitud_nuevo_usuario.status_code, 200)
+    def test_consultar_black_user(self):
+        solicitud_nuevo_usuario = self.client.get(
+            f"/blacklists/{self.email}",
+            headers={'Authorization': 'Bearer token'}
+        )
+        self.assertEqual(solicitud_nuevo_usuario.status_code, 200)
     
 
-    # def test_ping(self):
-    #     solicitud_nuevo_usuario = self.client.get(
-    #         f"/ping",
-    #         headers={'Authorization': 'Bearer token'}
-    #     )
-    #     self.assertEqual(solicitud_nuevo_usuario.status_code, 200)
+    def test_ping(self):
+        solicitud_nuevo_usuario = self.client.get(
+            f"/ping",
+            headers={'Authorization': 'Bearer token'}
+        )
+        self.assertEqual(solicitud_nuevo_usuario.status_code, 200)
